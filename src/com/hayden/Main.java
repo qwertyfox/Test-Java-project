b797eb2885471b4f60da50604c78afe21cc0cc4e
qwertyfox <67963327+qwertyfox@users.noreply.github.com>
2020-09-28 23:22:17 +1000

    Committed to git 2st

--- src/com/hayden/Main.java
+++ src/com/hayden/Main.java
@@ -1,8 +1,23 @@
 package com.hayden;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        List<Student> students = new LinkedList<Student>();
+        Student student = new Student(1,"Peter","Engineering");
+        Student student1 = new Student(2,"Pepper","Management");
+        Student student2 = new Student(3,"Rowan","Acting");
+        students.add(student);
+        students.add(student1);
+        students.add(student2);
+
+
+        for(Student s : students){
+            System.out.println(s.getName());
+        }
     }
 }
